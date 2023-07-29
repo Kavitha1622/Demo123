194b876c8f996a485587d5c4c9d6e6f2e40e4c20
kavitha <kavithaac2000@gmail.com>
2023-07-29 02:55:43 -0400

    Im learning Testing

---
+++ src/Demo123.java
@@ -0,0 +1,16 @@
+
+public class Demo123 
+{
+
+	public static void main(String[] args) 
+	{
+       int a;
+       float b;
+       String c;
+       a=Integer.parseInt(args[0]);
+       b=Float.parseFloat(args[1]);
+       c=(args[2]);
+       System.out.println("a="+a+"b="+b+"c="+c);
+	}
+
+}
